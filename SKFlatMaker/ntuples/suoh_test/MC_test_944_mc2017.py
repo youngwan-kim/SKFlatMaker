c43e538b148b3fff0444f69446f7d4269b7290f4
Jae Sung Kim <jskim@cern.ch>
2018-04-06 19:40:20 +0900

    revert..

--- SKFlatMaker/ntuples/suoh_test/MC_test_944_mc2017.py
+++ SKFlatMaker/ntuples/suoh_test/MC_test_944_mc2017.py
@@ -14,7 +14,6 @@ GT_MC = '94X_mc2017_realistic_v10'
 # TESTFILE_MC = 'file:/u/user/kplee/scratch/ROOTFiles_Test/80X/ExampleMiniAODv2_ZMuMuPowheg_M120to200_Moriond17.root' # -- no signal -- #
 #TESTFILE_MC = 'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/RECOSIMstep_94X_mc2017_realistic_v10-v1/00000/0293A280-B5F3-E711-8303-3417EBE33927.root' # -- a root file of DYJetsToLL_M-50, MG
 TESTFILE_MC = 'root://cms-xrd-global.cern.ch//store/mc/RunIIFall17MiniAOD/ZToEE_NNPDF31_13TeV-powheg_M_3500_4500/MINIAODSIM/94X_mc2017_realistic_v10-v2/00000/201990FB-B306-E811-9A19-782BCB678094.root' # -- a root file of /ZToEE_NNPDF31_13TeV-powheg_M_3500_4500/RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v2/MINIAODSIM
-TESTFILE_MC = '/store/user/jskim/TEST_CRAB/CMSSW_9_4_0_patch1_MINIAODSIM/180329_041623/0000/MINIAODSIM_1.root'
 TESTFILE_DATA = 'file:/afs/cern.ch/work/s/suoh/entuple_making/KPLee_code/CMSSW_9_4_2/src/Phys/SKFlatMaker/ntuples/suoh_test/17Nov17_Rereco/DoubleMuon/00FB06B4-0DDF-E711-9291-02163E012A3F.root'
 
 ####################################################################################################################
