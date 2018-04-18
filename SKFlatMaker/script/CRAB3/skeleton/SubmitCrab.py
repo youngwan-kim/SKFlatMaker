32b29b8f9a482ff100c20324b12387b2c07f5cbd
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-04-18 16:45:22 +0200

    crab submission script added

---
+++ SKFlatMaker/script/CRAB3/skeleton/SubmitCrab.py
@@ -0,0 +1,25 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+
+config = config()
+
+config.General.requestName = 'DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8' ## EDIT
+config.General.workArea = 'crab_projects'
+config.General.transferLogs = True
+config.General.transferOutputs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'RunSKFlatMaker.py'
+config.JobType.sendExternalFolder = True
+config.JobType.pyCfgParams = [] ## EDIT
+#config.JobType.numCores = 8
+#config.JobType.maxMemoryMB = 4000
+
+config.Data.inputDataset = '/DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v2/MINIAODSIM' ## EDIT
+
+config.Data.splitting = 'FileBased' ## EDIT
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/user/%s/SKFlat/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+config.Data.outputDatasetTag = 'SKFlatMaker_2017_v1'
+
+config.Site.storageSite = 'T3_KR_KISTI'
