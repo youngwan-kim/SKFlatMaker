15faca39212c52aae74b3ea35fa5e066f26aa207
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-09-06 11:34:24 +0200

    data json file fixed

--- SKFlatMaker/script/CRAB3/MakeCrab.py
+++ SKFlatMaker/script/CRAB3/MakeCrab.py
@@ -1,8 +1,10 @@
 import os
 
 #txtfilename = 'samplelist_MC.txt'
-txtfilename = 'samplelist_DATA_DoubleMuon.txt'
+#txtfilename = 'samplelist_DATA_DoubleMuon.txt'
 #txtfilename = 'samplelist_DATA_DoubleEG.txt'
+#txtfilename = 'sameplist_DATA_2016_TEST.txt'
+txtfilename = 'samplelist_PrivateMC.txt'
 
 SKFlatTag = os.environ['SKFlatTag']
 
@@ -97,7 +99,7 @@ for line in lines:
       out.write(sk_line)
 
   if isData=="DATA":
-    out.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Final/Cert_294927-306462_13TeV_PromptReco_Collisions17_JSON.txt'\n")
+    out.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'\n")
 
   if isData=="PrivateMC":
     out.write("config.Data.inputDBS = 'phys03'\n")
