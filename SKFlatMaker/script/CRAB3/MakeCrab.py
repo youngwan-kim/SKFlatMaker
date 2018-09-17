e2c9885d5d1bc9eee660b3ad00b8e18f4c22cdac
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-09-17 23:02:03 +0200

    CRAB script updated

--- SKFlatMaker/script/CRAB3/MakeCrab.py
+++ SKFlatMaker/script/CRAB3/MakeCrab.py
@@ -1,13 +1,20 @@
 import os
 
-txtfilename = 'samplelist_DATA_SingleMuon.txt'
-#txtfilename = 'samplelist_MC.txt'
-#txtfilename = 'samplelist_PrivateMC.txt'
+txtfilename = '2016_DATA.txt'
+#txtfilename = '2016_MC.txt'
+#txtfilename = '2017_DATA.txt'
+#txtfilename = '2017_MC.txt'
 
 SKFlatTag = os.environ['SKFlatTag']
 
+year = "-1"
+if "2016" in txtfilename:
+  year = "2016"
+if "2017" in txtfilename:
+  year = "2017"
+
 lines = open(txtfilename).readlines()
-pdfidshifts = open('PDFInfo.txt').readlines()
+pdfidshifts = open(year+'_PDFInfo.txt').readlines()
 
 str_sample = "DATA"
 isData = False
@@ -25,7 +32,7 @@ if ("Private" in txtfilename) or ("private" in txtfilename):
   isData = False
   isPrivateMC = True
 
-base_dir = SKFlatTag+'/crab_submission_'+str_sample+'/'
+base_dir = SKFlatTag+'/'+year+'/crab_submission_'+str_sample+'/'
 
 os.system('mkdir -p '+base_dir)
 os.system('cp RunSKFlatMaker.py '+base_dir)
@@ -74,7 +81,7 @@ for line in lines:
       else:
         out.write("config.General.requestName = '"+sample+"'\n")
     elif "config.JobType.pyCfgParams" in sk_line:
-      out.write("config.JobType.pyCfgParams = ['sampletype="+str_sample+"','PDFIDShift="+pdfshift+"','PDFOrder="+order+"','PDFType="+gentype+"']\n")
+      out.write("config.JobType.pyCfgParams = ['sampletype="+str_sample+"','PDFIDShift="+pdfshift+"','PDFOrder="+order+"','PDFType="+gentype+"','year="+year+"']\n")
     elif "config.Data.inputDataset" in sk_line:
       out.write("config.Data.inputDataset = '"+line+"'\n")
     elif 'config.Data.splitting' in sk_line:
@@ -90,19 +97,22 @@ for line in lines:
     elif 'config.Data.outputDatasetTag' in sk_line:
       if isData:
         period = ""
-        if "2017B" in confs:
+        if year+"B" in confs:
           period = "B"
-        elif "2017C" in confs:
+        elif year+"C" in confs:
           period = "C"
-        elif "2017D" in confs:
+        elif year+"CD" in confs:
           period = "D"
-        elif "2017E" in confs:
+        elif year+"CE" in confs:
           period = "E"
-        elif "2017F" in confs:
+        elif year+"CF" in confs:
           period = "F"
         out.write("config.Data.outputDatasetTag = 'SKFlat_"+SKFlatTag+"_period"+period+"'\n")
       else:
         out.write("config.Data.outputDatasetTag = 'SKFlat_"+SKFlatTag+"'\n")
+
+    elif 'config.Data.outLFNDirBase' in sk_line:
+      out.write("config.Data.outLFNDirBase = '/store/user/%s/SKFlat/"+year+"/' % (getUsernameFromSiteDB())\n")
 
     elif 'config.Site.storageSite' in sk_line:
       if isPrivateMC:
@@ -113,7 +123,10 @@ for line in lines:
       out.write(sk_line)
 
   if isData:
-    out.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'\n")
+    if year=="2016":
+      out.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'\n")
+    if year=="2017":
+      out.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'\n")
 
   if isPrivateMC:
     out.write("config.Data.inputDBS = 'phys03'\n")
