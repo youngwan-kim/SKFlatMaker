4efff569d5c6348ccb29358f281b6f8b8d9f9e88
Jae Sung Kim <jae.sung.kim@cern.ch>
2019-10-13 08:05:40 +0200

    2016 JSON updated

--- SKFlatMaker/script/CRAB3/MakeCrab.py
+++ SKFlatMaker/script/CRAB3/MakeCrab.py
@@ -182,7 +182,7 @@ for line in lines:
 
   if isData:
     if year=="2016":
-      out.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'\n")
+      out.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_ReReco_07Aug2017_Collisions16_JSON.txt'\n")
     elif year=="2017":
       out.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'\n")
     elif year=="2018":
